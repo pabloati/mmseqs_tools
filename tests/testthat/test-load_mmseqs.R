6f29f201152f4d99d421ac9db2cdd011d9f0f403
Pablo Atienza <pabloatienzalo@gmail.com>
2023-11-30 16:41:34 +0100

    Updated tests

--- tests/testthat/test-load_mmseqs.R
+++ tests/testthat/test-load_mmseqs.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("loads the file", {
+  load_mmseqs(file = "test_data/test_lca.tsv")
+})
+
+test_that("Sample name option works", {
+  df <- load_mmseqs(file = "test_data/test_lca.tsv", include_name = TRUE)
+  expect_equal(unique(df$sample),"test")
+})
+
+test_that("Filtering works", {
+  df <- load_mmseqs(file = "test_data/test_lca.tsv", frag_thr = 2)
+  expect_equal(min(df$total_fragments),2)
 })
